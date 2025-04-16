ec9a6b45788ff3949801ea97cece3196f802698d
ericcarry88828 <ericcarry88828@gmail.com>
2025-04-16 21:56:42 +0800

    create route, controller, insert data to db

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -14,6 +17,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
+app.use('/api/v1/products', productsRouter);
+
 // products route
 
 app.use(notFoundMiddleware);
@@ -23,6 +28,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening port ${port}...`));
   } catch (error) {
     console.log(error);
---
+++ controllers/products.js
@@ -0,0 +1,13 @@
+const { createCustomError } = require('../errors/custom-error');
+const getAllProductsStatic = async (req, res) => {
+  res.status(200).json({ msg: 'products testing route' });
+};
+
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: 'products route' });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
