b70b6be7f0489a878ffec60ba07ed283074f7126
ericcarry88828 <ericcarry88828@gmail.com>
2025-04-19 20:25:42 +0800

    finish select limit skip pagenation

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,50 @@
+const Product = require('../models/product');
 const { createCustomError } = require('../errors/custom-error');
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: 'products testing route' });
+  const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10)
+    .skip(0);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  let result = Product.find(queryObject);
+  // sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createAt');
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
